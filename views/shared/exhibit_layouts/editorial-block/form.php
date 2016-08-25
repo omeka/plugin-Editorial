6a3b150cfdaa355b7accb06718c1d1748261737e
patrickmj <patrickmjchnm@gmail.com>
2016-08-25 14:00:24 -0400

    cherrypick changes in responses branch that should've been in master

--- views/shared/exhibit_layouts/editorial-block/form.php
+++ views/shared/exhibit_layouts/editorial-block/form.php
@@ -6,11 +6,45 @@ $currentUser = current_user();
 
 unset ($usersForSelect[$currentUser->id]);
 
+// allow some users to change who's allowed access
+// this could/should be done by a fancy ACL class thing,
+// but it's quicker and cheaper this way. I'm ok with this technical debt for now
+// see #7
+
+$changeAllowed = false;
+
+if (   $currentUser->role == 'admin'
+    || $currentUser->role == 'super'
+    || $currentUser->id == $block->owner_id
+   ) {
+    
+    $changeAllowed = true;
+}
+
+
 ?>
 
 <div class="block-text">
-    <h4><?php echo __('Text'); ?></h4>
+    <h4><?php echo __('Comment'); ?></h4>
     <?php echo $this->exhibitFormText($block); ?>
+    <?php if ($block->exists()): ?>
+    <?php $responses = get_db()->getTable('EditorialBlockResponse')->findBy(array('block_id' => $block->id)); ?>
+    <div class='editorial-block-responses'>
+        <?php foreach ($responses as $response): ?>
+        <div>
+        
+        </div>
+        <?php endforeach; ?>
+        <div class='editorial-block-response new' style='margin-left: 25px; '>
+            <?php 
+                echo $this->formLabel($formStem . '[options][response]', 'Leave new response');
+                echo $this->formTextarea($formStem . '[options][response]');
+            ?>
+        </div>
+    </div>
+
+    
+    <?php endif; ?>
 </div>
 
 <div class='layout-options'>
@@ -26,11 +60,16 @@ unset ($usersForSelect[$currentUser->id]);
         ?>
     </div>
     <div class='users-select'>
-        
+        <?php 
+            $selectAttrs = array('multiple' => true, 'size' => 10);
+            if (!$changeAllowed) {
+                $selectAttrs['disabled'] = 'disabled';
+            }
+        ?>
         <?php echo $this->formLabel($formStem . '[options][users]', __('Allowed Users')); ?>
         <?php echo $this->formSelect($formStem . '[options][users]',
                                      @$options['users'],
-                                     array('multiple' => true, 'size' => 10),
+                                     $selectAttrs,
                                      $usersForSelect
                 ); ?>
     </div>
