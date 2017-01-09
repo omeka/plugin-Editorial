0da34ad856f58926c314b2e480de3d9b386f0ed9
patrickmj <patrickmjchnm@gmail.com>
2017-01-09 15:40:31 -0500

    add link to email.
    close #42

--- EditorialPlugin.php
+++ EditorialPlugin.php
@@ -296,7 +296,7 @@ class EditorialPlugin extends Omeka_Plugin_AbstractPlugin
         $body .= '<br/>';
         $body .= snippet($block->text, 0, 250);
 
-        $body .= '<p>'.__('View the page at ').record_url($block->getPage(), 'edit-page', true).'</p>';
+        $body .= "<p><a href='".record_url($block->getPage(), 'edit-page', true)."'>".__('View the page at ').record_url($block->getPage(), 'edit-page', true).'</p>';
         $mail->setSubject($subject);
         $mail->setBodyHtml($body);
         try {
