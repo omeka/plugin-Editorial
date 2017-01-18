ea649836e473edf6b96659f25515a9f2302ccc98
patrickmj <patrickmjchnm@gmail.com>
2017-01-18 16:36:09 -0500

    cleanup no-longer needed timestamp info on blocks
    this data is now in EB master

--- models/EditorialBlockInfo.php
+++ models/EditorialBlockInfo.php
@@ -5,15 +5,6 @@ class EditorialBlockInfo extends Omeka_Record_AbstractRecord
     public $block_id;
 
     public $owner_id;
-    
-    public $added;
-    
-    public $modified;
-    
-    public function _initializeMixins()
-    {
-        $this->_mixins[] = new Mixin_Timestamp($this);
-    }
 
     public function getOwner()
     {
